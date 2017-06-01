372fbc07080b44a91e3c44699666f48a8674f079
samshipengs <samshipengs@hotmail.com>
2017-06-01 17:22:18 -0400

    Start SVM

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -21,7 +21,6 @@ def svm_loss_naive(W, X, y, reg):
   - gradient with respect to weights W; an array of same shape as W
   """
   dW = np.zeros(W.shape) # initialize the gradient as zero
-
   # compute the loss and the gradient
   num_classes = W.shape[1]
   num_train = X.shape[0]
@@ -35,6 +34,11 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:, y[i]] -= X[i]
+        dW[:, j] += X[i]
+
+  dW /= num_train
+  dW += 2.*reg*np.abs(W)
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -70,7 +74,7 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  W*X-W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
